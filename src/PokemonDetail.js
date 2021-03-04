faf2a3759ff6ccee5f03e5a085a11e4ecb55393b
Muhammad Naufal <mnaufal75@gmail.com>
2021-03-04 14:44:04 +0700

    Add color button based on Pokemon type

--- src/PokemonDetail.js
+++ src/PokemonDetail.js
@@ -27,6 +27,27 @@ const GET_POKEMON_DETAIL = gql`
   }
 `
 
+const pokemonType = {
+  'bug': '#729f3f',
+  'dark': '#707070',
+  'dragon': '#53a4cf',
+  'electric': '#eed535',
+  'fairy': '#fdb9e9',
+  'fighting': '#d56723',
+  'fire': '#fd7d24',
+  'flying': '#3dc7ef',
+  'ghost': '#7b62a3',
+  'grass': '#9bcc50',
+  'ground': '#f7de3f',
+  'ice': '#51c4e7',
+  'normal': '#a4acaf',
+  'poison': '#b97fc9',
+  'psychic': '#f366b9',
+  'rock': '#a38c21',
+  'steel': '#9eb7b8',
+  'water': '#4592c4',
+}
+
 const PokemonDetail = ({ myPokemon, handleSetMyPokemon }) => {
   const [nickname, setNickname] = useState("");
   const [showModal, setShowmodal] = useState(false);
@@ -158,17 +179,21 @@ const PokemonDetail = ({ myPokemon, handleSetMyPokemon }) => {
             .map(type => {
               return <span key={type.type.name} css={css`
                 font-size: 1.2rem;
-                padding: 1rem;
+                margin: 1rem;
+                padding: 0.5rem;
+                border-radius: 0.25rem;
                 text-transform: capitalize;
+                color: white;
+                background-color: ${pokemonType[type.type.name]};
               `}>{type.type.name}</span>;
             })
           }
         </div>
       </div>
 
-      <div css={css`padding: 1rem;`}>
+      <div css={css`padding: 1rem; `}>
         <span css={css`
-          text-align: left;
+              text - align: left;
           font-size: 1.5rem;
         `}>
           Moves:
