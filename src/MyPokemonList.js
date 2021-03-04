93c91e8d200a6016cdf7cb3ec14ffe6669cc2880
Muhammad Naufal <mnaufal75@gmail.com>
2021-03-04 14:58:00 +0700

    Add ellipsis (...) for long name in MyPokemonList

--- src/MyPokemonList.js
+++ src/MyPokemonList.js
@@ -17,17 +17,26 @@ const MyPokemonList = ({ myPokemon, handleRemovePokemon }) => {
         {
           myPokemon.map((pokemon) => (
             <li css={css`text-align: left; padding: 0.2rem; font-size: 1.5rem`}>
-              <span css={css`
-                text-align: left;
-                display: inline-block;
-                font-size: 1.5rem;
-              `}>{pokemon}</span>
-              <button css={css`
-                width: 150px;
-                font-size: 1.5rem;
-                float: right;
-                border: 0;
-              `} onClick={() => removePokemon(pokemon)}>Remove</button>
+              <div css={css`display: flex; width: 100%`}>
+                <div css={css`
+                  text-align: left;
+                  display: inline-block;
+                  font-size: 1.5rem;
+                  flex: 1;
+                  overflow: hidden;
+                  text-overflow: ellipsis;
+                `}>{pokemon}</div>
+                <div css={css`
+                  flex: 0;
+                `}>
+                  <button css={css`
+                    width: 150px;
+                    font-size: 1.5rem;
+                    float: right;
+                    border: 0;
+                  `} onClick={() => removePokemon(pokemon)}>Remove</button>
+                </div>
+              </div>
             </li>
           ))
         }
