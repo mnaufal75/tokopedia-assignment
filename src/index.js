041437dcf932d4766f2c7792bda64091257fb03d
Muhammad Naufal <mnaufal75@gmail.com>
2021-03-04 14:18:36 +0700

    Remove index.css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
+/** @jsxImportSource @emotion/react */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import { Global, css } from '@emotion/react'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <Global styles={css`
+      * {
+        box-sizing: border-box;
+      }
+      body {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+          "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+          sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+      }
+      code {
+        font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+          monospace;
+      }
+    `}
+      />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
